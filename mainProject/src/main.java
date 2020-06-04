a4aef052a60c1f6cc92143b395fe3a483ec7f272
Andrew <andrewsagorski@live.com>
2020-06-04 13:10:09 +1000

    New changes in trying to convert image data to an image.

--- mainProject/src/main.java
+++ mainProject/src/main.java
@@ -1,17 +1,17 @@
-
-import ControlPanel.Login.GUI.loginScreen;
-import ControlPanel.Home.GUI.UserManagement.userManagement;
-import Server.*;
-import ControlPanel.Utility.billboard;
-
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.ArrayList;
-
-class main{
-    public static void main(String[] args) throws NoSuchAlgorithmException, SQLException, IOException {
-        ControlPanel.Login.GUI.loginScreen.controlPanelLogin();
-    }
-}
+//
+//import ControlPanel.Login.GUI.loginScreen;
+//import ControlPanel.Home.GUI.UserManagement.userManagement;
+//import Server.*;
+//import ControlPanel.Utility.billboard;
+//
+//import java.io.IOException;
+//import java.security.NoSuchAlgorithmException;
+//import java.sql.Connection;
+//import java.sql.SQLException;
+//import java.util.ArrayList;
+//
+//class main{
+//    public static void main(String[] args) throws NoSuchAlgorithmException, SQLException, IOException {
+//        ControlPanel.Login.GUI.loginScreen.controlPanelLogin();
+//    }
+//}
