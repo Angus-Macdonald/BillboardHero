90ce7b59ce72e60f1f7203cb3eeddd323d4b6442
angusmacdonald1996 <n10448888@qut.edu.au>
2020-06-05 23:27:59 +1000

    Java comments

--- mainProject/src/ControlPanel/Utility/FrameAndPanelUtility.java
+++ mainProject/src/ControlPanel/Utility/FrameAndPanelUtility.java
@@ -5,6 +5,12 @@ import java.awt.*;
 
 public class FrameAndPanelUtility {
 
+    /**
+     * This function takes a JFrame and applies some recurred functions that were used. This in comparison to the next function only changes the Close window and screen size,
+     * where the latter will also create a frame GridLayout.
+     * @param frame
+     * @return
+     */
     public static Dimension frameManage(JFrame frame){
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
@@ -12,6 +18,12 @@ public class FrameAndPanelUtility {
         return dim;
     }
 
+    /**
+     * This function does the same as the last, except is also takes 2 ints in which allow the user to create the frame with a grid layout.
+     * @param frame
+     * @param rows
+     * @param columns
+     */
     public static void frameManage(JFrame frame, int rows, int columns){
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
@@ -19,6 +31,10 @@ public class FrameAndPanelUtility {
         frame.setLayout(new GridLayout(rows, columns));
     }
 
+    /**
+     * This function eliminates panel initialisation recursion by taking a JPanel[] and initialising all the JPanels
+     * @param panel
+     */
     public static void panelInitialise(JPanel[] panel){
         for(int i = 0; i < panel.length; i++){
             panel[i] = new JPanel();
